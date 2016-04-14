b8c093c9d07b834c8d2fcb440a069e9fd2abab4f
Olga Saliy <olia.white.1996@gmail.com>
2016-04-14 14:39:16 +0300

    sixth task

--- interfaceWrapper/ru/application.js
+++ interfaceWrapper/ru/application.js
@@ -6,3 +6,10 @@ console.log('Application going to read ' + fileName);
 fs.readFile(fileName, function(err, src) {
   console.log('File ' + fileName + ' size ' + src.length);
 });
+
+var path = 'tmp';
+fs.mkdir(path, function (err) {
+  if (err) {
+    console.log('failed to create directory', err);
+  }
+});
